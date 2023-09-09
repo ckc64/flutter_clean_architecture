873f440f0921d75c005664aa5f8af60285ac3db6
ckc64 <charles.caber@achievewithoutborders.com>
2023-09-09 16:38:58 +0800

    added domain data

---
+++ news_app_clean_architecture/lib/features/daily_news/data/repository/article_repository_implementation.dart
@@ -0,0 +1,33 @@
+import 'dart:io';
+
+import 'package:dio/dio.dart';
+import 'package:news_app_clean_architecture/core/constants/constants.dart';
+import 'package:news_app_clean_architecture/core/resources/data_state.dart';
+import 'package:news_app_clean_architecture/features/daily_news/data/data_sources/remote/news_api_service.dart';
+import 'package:news_app_clean_architecture/features/daily_news/data/models/article.dart';
+import 'package:news_app_clean_architecture/features/daily_news/domain/repositories/article_repository.dart';
+
+class ArticleRepositoryImpl implements ArticleRepository {
+  final NewsApiService _newsApiService;
+  ArticleRepositoryImpl(this._newsApiService);
+
+  @override
+  Future<DataState<List<ArticleModel>>> getNewsArticle() async {
+    try {
+      final httpResponse = await _newsApiService.getNewsArticles(
+          apiKey: newsAPIKey, category: categoryQuery, country: countryQuery);
+
+      if (httpResponse.response.statusCode == HttpStatus.ok) {
+        return DataSuccess(httpResponse.data);
+      } else {
+        return DataFailed(DioException(
+            requestOptions: httpResponse.response.requestOptions,
+            error: httpResponse.response.statusCode,
+            response: httpResponse.response,
+            type: DioExceptionType.badResponse));
+      }
+    } on DioException catch (e) {
+      return DataFailed(e);
+    }
+  }
+}
