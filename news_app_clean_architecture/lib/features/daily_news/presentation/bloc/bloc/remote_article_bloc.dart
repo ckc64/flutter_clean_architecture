11afbdfc0ab4a32911858b547e601dcd56847b32
ckc64 <charles.caber@achievewithoutborders.com>
2023-09-10 18:10:59 +0800

    added bloc

---
+++ news_app_clean_architecture/lib/features/daily_news/presentation/bloc/bloc/remote_article_bloc.dart
@@ -0,0 +1,26 @@
+import 'package:bloc/bloc.dart';
+import 'package:news_app_clean_architecture/core/resources/data_state.dart';
+import 'package:news_app_clean_architecture/features/daily_news/domain/use_cases/get_articles.dart';
+import 'remote_article_event.dart';
+import 'remote_article_state.dart';
+
+class RemoteArticleBloc extends Bloc<RemoteArticlesEvent, RemoteArticleState> {
+  final GetArticleUseCase _getArticleUseCase;
+
+  RemoteArticleBloc(this._getArticleUseCase)
+      : super(const RemoteArticlesLoading()) {
+    on<GetArticles>(onGetArticles);
+  }
+
+  void onGetArticles(
+      GetArticles event, Emitter<RemoteArticleState> emit) async {
+    final dataState = await _getArticleUseCase();
+
+    if (dataState is DataSuccess && dataState.data!.isNotEmpty) {
+      emit(RemoteArticlesDone(dataState.data!));
+    }
+    if (dataState is DataFailed) {
+      emit(RemoteArticlesError(dataState.error!));
+    }
+  }
+}
